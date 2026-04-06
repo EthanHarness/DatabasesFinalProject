6146b7f3f3787312b4a523f18acf75a4003ff40d
Ethan Harness <EthanMichaelHarness@gmail.com>
2026-04-06 12:31:06 -0600

    Refactor of Parser.

--- JsonParser/JsonParser.py
+++ JsonParser/JsonParser.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from LexerTokens import Whitespace, Number, StringT, TrueValue, FalseValue, NullValue, \
+from LexerTokens import Token, Whitespace, Number, StringT, TrueValue, FalseValue, NullValue, \
     LBracket, RBracket, Comma, Colon, LBrace, RBrace
 from JsonLexer import Lexer, LexerToken
 
@@ -14,50 +14,40 @@ class Parser:
         self.tokenization: List[LexerToken] = lexerTokenization
         self.locationInTokenization: int = 0 
     
-    def matchToken(self, expectedTokenString: str, token: LexerToken) -> None:
-        if token.type.TOKEN_STRING != expectedTokenString:
+    def matchToken(self, expectedTokenString: str) -> None:
+        if self.tokenization[self.locationInTokenization].type.TOKEN_STRING != expectedTokenString:
             raise Exception("Failed to parse")
+        self.locationInTokenization += 1
+        
+    def matchIf(self, tokenStr: str) -> None:
+        if self.tokenization[self.locationInTokenization].type.TOKEN_STRING == tokenStr:
+            self.matchToken(tokenStr)
+
         
     def ParseObject(self) -> None:
         startTokenIndex = self.locationInTokenization 
-
-        self.matchToken(LBrace.TOKEN_STRING, self.tokenization[self.locationInTokenization])
-        self.locationInTokenization += 1
-
-        if self.tokenization[self.locationInTokenization].type.TOKEN_STRING == Whitespace.TOKEN_STRING:
-            self.matchToken(Whitespace.TOKEN_STRING, self.tokenization[self.locationInTokenization])
-            self.locationInTokenization += 1
+    
+        self.matchToken(LBrace.TOKEN_STRING)
+        self.matchIf(Whitespace.TOKEN_STRING)
 
         if self.tokenization[self.locationInTokenization].type.TOKEN_STRING == RBrace.TOKEN_STRING:
-            self.matchToken(RBrace.TOKEN_STRING, self.tokenization[self.locationInTokenization])
-            self.locationInTokenization += 1
+            self.matchToken(RBrace.TOKEN_STRING)
             return
 
         while(True):
-            self.matchToken(StringT.TOKEN_STRING, self.tokenization[self.locationInTokenization])
-            self.locationInTokenization += 1
-
-            if self.tokenization[self.locationInTokenization].type.TOKEN_STRING == Whitespace.TOKEN_STRING:
-                self.matchToken(Whitespace.TOKEN_STRING, self.tokenization[self.locationInTokenization])
-                self.locationInTokenization += 1
-
-            self.matchToken(Colon.TOKEN_STRING, self.tokenization[self.locationInTokenization])
-            self.locationInTokenization += 1
+            self.matchToken(StringT.TOKEN_STRING)
+            self.matchIf(Whitespace.TOKEN_STRING)
+            self.matchToken(Colon.TOKEN_STRING)
 
             self.ParseValue() #Will take care of leading and trailing white space
 
             if self.tokenization[self.locationInTokenization].type.TOKEN_STRING != Comma.TOKEN_STRING:
                 break
+            
+            self.matchToken(Comma.TOKEN_STRING)
+            self.matchIf(Whitespace.TOKEN_STRING)
 
-            self.matchToken(Comma.TOKEN_STRING, self.tokenization[self.locationInTokenization])
-            self.locationInTokenization += 1
-
-            if self.tokenization[self.locationInTokenization].type.TOKEN_STRING == Whitespace.TOKEN_STRING:
-                self.matchToken(Whitespace.TOKEN_STRING, self.tokenization[self.locationInTokenization])
-                self.locationInTokenization += 1
-
-        self.matchToken(RBrace.TOKEN_STRING, self.tokenization[self.locationInTokenization])
-        self.locationInTokenization += 1
+        self.matchToken(RBrace.TOKEN_STRING)
 
         print(f"Object has tokens from index {startTokenIndex} to index {self.locationInTokenization-1}")
         if self.locationInTokenization == len(self.tokenization):
@@ -66,59 +56,32 @@ class Parser:
     def ParseArray(self) -> None:
         startTokenIndex = self.locationInTokenization 
 
-        self.matchToken(LBracket.TOKEN_STRING, self.tokenization[self.locationInTokenization])
-        self.locationInTokenization += 1
-
-        if self.tokenization[self.locationInTokenization].type.TOKEN_STRING == Whitespace.TOKEN_STRING:
-            self.matchToken(Whitespace.TOKEN_STRING, self.tokenization[self.locationInTokenization])
-            self.locationInTokenization += 1
-
-        if self.tokenization[self.locationInTokenization].type.TOKEN_STRING == RBracket.TOKEN_STRING:
-            self.matchToken(RBracket.TOKEN_STRING, self.tokenization[self.locationInTokenization])
-            self.locationInTokenization += 1
+        self.matchToken(LBracket.TOKEN_STRING)
+        self.matchIf(Whitespace.TOKEN_STRING)
+        self.matchIf(RBracket.TOKEN_STRING)
 
         while(True):
             self.ParseValue() #Will take care of leading and trailing white space
 
             if self.tokenization[self.locationInTokenization].type.TOKEN_STRING != Comma.TOKEN_STRING:
                 break
+            
+            self.matchToken(Comma.TOKEN_STRING)
 
-            self.matchToken(Comma.TOKEN_STRING, self.tokenization[self.locationInTokenization])
-            self.locationInTokenization += 1
-
-        self.matchToken(RBracket.TOKEN_STRING, self.tokenization[self.locationInTokenization])
-        self.locationInTokenization += 1
+        self.matchToken(RBracket.TOKEN_STRING)
 
         print(f"Array has tokens from index {startTokenIndex} to index {self.locationInTokenization-1}")
         
     def ParseValue(self) -> None:
         startTokenIndex = self.locationInTokenization
 
-        if self.tokenization[self.locationInTokenization].type.TOKEN_STRING == Whitespace.TOKEN_STRING:
-            self.matchToken(Whitespace.TOKEN_STRING, self.tokenization[self.locationInTokenization])
-            self.locationInTokenization += 1
-
+        self.matchIf(Whitespace.TOKEN_STRING)
         match self.tokenization[self.locationInTokenization].type.TOKEN_STRING:
-
-            case StringT.TOKEN_STRING:
-                self.matchToken(StringT.TOKEN_STRING, self.tokenization[self.locationInTokenization])
-                self.locationInTokenization += 1
-
-            case Number.TOKEN_STRING:
-                self.matchToken(Number.TOKEN_STRING, self.tokenization[self.locationInTokenization])
-                self.locationInTokenization += 1
-
-            case TrueValue.TOKEN_STRING:
-                self.matchToken(TrueValue.TOKEN_STRING, self.tokenization[self.locationInTokenization])
-                self.locationInTokenization += 1
-
-            case FalseValue.TOKEN_STRING:
-                self.matchToken(FalseValue.TOKEN_STRING, self.tokenization[self.locationInTokenization])
-                self.locationInTokenization += 1
-
-            case NullValue.TOKEN_STRING:
-                self.matchToken(NullValue.TOKEN_STRING, self.tokenization[self.locationInTokenization])
-                self.locationInTokenization += 1
+            case StringT.TOKEN_STRING: self.matchToken(StringT.TOKEN_STRING)
+            case Number.TOKEN_STRING: self.matchToken(Number.TOKEN_STRING)
+            case TrueValue.TOKEN_STRING: self.matchToken(TrueValue.TOKEN_STRING)
+            case FalseValue.TOKEN_STRING: self.matchToken(FalseValue.TOKEN_STRING)
+            case NullValue.TOKEN_STRING: self.matchToken(NullValue.TOKEN_STRING)
 
             case _:
                 try:
@@ -126,10 +89,7 @@ class Parser:
                 except: #If object fails then try array. If array fails then we should throw error anyways. 
                     self.ParseArray()
 
-        if self.tokenization[self.locationInTokenization].type.TOKEN_STRING == Whitespace.TOKEN_STRING:
-            self.matchToken(Whitespace.TOKEN_STRING, self.tokenization[self.locationInTokenization])
-            self.locationInTokenization += 1
-
+        self.matchIf(Whitespace.TOKEN_STRING)
         print(f"Value has tokens from index {startTokenIndex} to index {self.locationInTokenization-1}")
     
         
